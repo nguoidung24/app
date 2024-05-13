7eac0a18906ab08739f11edbd0222917e33e27f3
dungTam <dungtamtithoi88@gmail.com>
2024-05-13 16:02:03 +0700

    end game

--- components/Index/OutstandingProduct.vue
+++ components/Index/OutstandingProduct.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="relative bg-gray-50 rounded-2xl pt-6">
-        <div class="font-semibold w-full text-lg text-center  lg:px-10 px-3"
-            style="z-index: 99999;">
+        <div class="font-semibold w-full text-lg text-center  lg:px-10 px-3" style="z-index: 99999;">
             <Swiper :navigation="true" :modules="modules" :spaceBetween="10" :breakpoints="{
                 '0': {
                     slidesPerView: 3,
@@ -17,7 +16,9 @@
                 },
             }" class="lg:*:justify-center">
                 <SwiperSlide v-for="(item, index) in dataTab" :key="index">
-                    <button @click="slideTo(index)" :class="tabActive == index ? 'border-b-2 border-black' : ' hover:border-b-2  hover:text-gray-500'">{{item.name}}</button>
+                    <button @click="slideTo(index)"
+                        :class="tabActive == index ? 'border-b-2 border-black' : ' hover:border-b-2  hover:text-gray-500'">{{
+                            item.name }}</button>
                 </SwiperSlide>
             </Swiper>
 
@@ -25,13 +26,38 @@
         <div v-for="(item, index) in dataTab" :key="index">
             <div v-if="tabActive == index"
                 class="relative grid grid-cols-1 lg:grid-cols-2 md:grid-cols-2 p-6 gap-3 mb-2">
-                <div class='  rounded-3xl text-white text-center overflow-hidden'>
-                    <img :src="item.bigImage" alt="">
+                <div class='rounded-3xl text-black text-center overflow-hidden'>
+                    <figure class="relative">
+                        <img :src="item.bigImage" alt="">
+                        <p class="absolute bottom-10 left-2/4  -translate-x-2/4 font-bold text-xl">
+                            {{ item.bigText }}
+                        </p>
+                        <div class="w-full h-full peer absolute top-0 left-0">
+                        </div>
+
+                        <NuxtLink to="/"
+                            class="absolute -bottom-full peer-hover:bottom-10 hover:bottom-10 left-2/4 bg-black text-white px-10 rounded-3xl lg:w-fit text-base w-[70%] text-sm text-center duration-500 py-3 -translate-x-2/4">
+                            {{ item.button }}
+                        </NuxtLink>
+                    </figure>
                 </div>
                 <div class='  rounded-3xl grid grid-cols-2 gap-3 grid-rows-2'>
                     <div v-for="(value, key) in item.smallImage" :key="key"
-                        class="w-full h-full  rounded-3xl overflow-hidden">
-                        <img :src="value" alt="">
+                        class="w-full h-full rounded-3xl  overflow-hidden">
+                        <figure class="relative ">
+                            <img :src="value" alt="">
+                            <p
+                                class="absolute lg:bottom-10  bottom-2 text-center font-bold lg:text-sm md:text-sm text-[13px] w-full px-10 left-2/4 -translate-x-2/4">
+                                {{ item.smaillText[key] }}
+                            </p>
+                            <div class="w-full h-full peer absolute top-0 left-0">
+                            </div>
+
+                            <NuxtLink to="/"
+                                class="absolute -bottom-full peer-hover:bottom-10 hover:bottom-10 left-2/4 bg-black text-white px-10 rounded-3xl lg:w-fit text-base w-[70%] text-sm text-center duration-500 py-3 -translate-x-2/4">
+                                {{ item.button }}
+                            </NuxtLink>
+                        </figure>
                     </div>
                 </div>
             </div>
@@ -46,44 +72,76 @@ import "swiper/css"
 import 'swiper/css/navigation';
 
 const dataTab = [
-    {   
+    {
         name: "Ưu đãi",
+        bigText: "65 Inch The Frame LS03D QLED 4K Smart TV (2024)",
+        button: "Mua ngay",
         bigImage: "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/684-684-0506.jpg?$684_684_JPG$",
         smallImage: [
             "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/S24ultra-exnogift-PC.jpg?$330_330_JPG$s",
             "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/PC-Small-F-AR13DYHZA241.jpg?$330_330_JPG$",
             "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/S24series-nogift-PC.jpg?$330_330_JPG$",
             "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/330-330-0506.jpg?$330_330_JPG$"
+        ],
+        smaillText: [
+            "Galalxy S24 Ultra màu độc quyền. Tặng ngay Buds FE vs Sạc 25W",
+            "Galaxy S24 | S24+ màu độc quyền. Tặng ngay Buds FE và Sạc 25W",
+            "Điều hòa Digital Inverter 12,000 BTU/h",
+            "Loa Tranh Music Frame HW-LS60D (2024)"
         ]
     },
     {
         name: "TiVi",
+        bigText: "65 Inch The Frame LS03D QLED 4K Smart TV (2024)",
+        button: "Mua ngay",
         bigImage: "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/Big_PC_UA58AU7000KXXV.jpg?$684_684_JPG$",
         smallImage: [
             "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/Small_PC_QA65QE1CAKXXV2.jpg?$330_330_JPG$",
             "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/Small_PC_QA55Q70CAKXXV2.jpg?$330_330_JPG$",
             "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/Small_PC_QA65LS03DAKXXV1.jpg?$330_330_JPG$",
             "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/Small_PC_UA65BU8000KXXV1.jpg?$330_330_JPG$"
+        ],
+        smaillText: [
+            "Galalxy S24 Ultra màu độc quyền. Tặng ngay Buds FE vs Sạc 25W",
+            "Galaxy S24 | S24+ màu độc quyền. Tặng ngay Buds FE và Sạc 25W",
+            "Điều hòa Digital Inverter 12,000 BTU/h",
+            "Loa Tranh Music Frame HW-LS60D (2024)"
         ]
     },
     {
         name: "Điện thoại",
+        bigText: "65 Inch The Frame LS03D QLED 4K Smart TV (2024)",
+        button: "Mua ngay",
         bigImage: "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/S23FE-nogift-PC.jpg?$684_684_JPG$",
         smallImage: [
             "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/ZFlip5-nogift-PC.jpg?$330_330_JPG$",
             "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/A55-5G-pc-0429.jpg?$330_330_JPG$",
             "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/A35-pc-0429.jpg?$330_330_JPG$",
             "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/Fit3-nogift-PC.jpg?$330_330_JPG$"
+        ],
+        smaillText: [
+            "Galalxy S24 Ultra màu độc quyền. Tặng ngay Buds FE vs Sạc 25W",
+            "Galaxy S24 | S24+ màu độc quyền. Tặng ngay Buds FE và Sạc 25W",
+            "Điều hòa Digital Inverter 12,000 BTU/h",
+            "Loa Tranh Music Frame HW-LS60D (2024)"
         ]
     },
     {
         name: "Gia dụng",
+        bigText: "65 Inch The Frame LS03D QLED 4K Smart TV (2024)",
+        button: "Mua ngay",
         bigImage: "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/Big_PC_F-AR13DYHZA24_.jpg?$684_684_JPG$",
         smallImage: [
             "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/Small_PC_RB30N4170BU_SV.jpg?$330_330_JPG$",
             "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/Small_PC_DV90BB9440GHSV.png?$330_330_PNG$",
             "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/Small_PC_RF48A4010B4_SV_.jpg?$330_330_JPG$",
             "https://images.samsung.com/is/image/samsung/assets/vn/home/2024/Small_PC_F-DW60CB750S241.jpg?$330_330_JPG$"
+        ],
+        smaillText: [
+            "Galalxy S24 Ultra màu độc quyền. Tặng ngay Buds FE vs Sạc 25W",
+            "Galaxy S24 | S24+ màu độc quyền. Tặng ngay Buds FE và Sạc 25W",
+            "Điều hòa Digital Inverter 12,000 BTU/h",
+            "Loa Tranh Music Frame HW-LS60D (2024)"
         ]
     }
 ]
@@ -107,8 +165,8 @@ export default defineNuxtComponent({
         };
     },
     methods: {
-        slideTo(index){
-            this.tabActive = index 
+        slideTo(index) {
+            this.tabActive = index
         }
     }
 })
