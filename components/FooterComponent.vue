5de25fc84b09bef16da5facba37ce84858cd19ce
dungTam <dungtamtithoi88@gmail.com>
2024-05-13 11:33:15 +0700

    add footer

--- components/FooterComponent.vue
+++ components/FooterComponent.vue
@@ -8,5 +8,34 @@ export default {
 </script>
 <template>
     <div>
+        
+
+<footer class=" shadow mt-4">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div class="sm:flex sm:items-center sm:justify-between">
+            <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src="https://i.postimg.cc/MZCBXb1K/logo.png" class="h-12" alt="LOGO" srcset="">
+            </a>
+            <ul class="flex flex-wrap items-center mb-6 text-sm font-medium sm:mb-0 ">
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm  sm:text-center ">© 2024 <a href="https://flowbite.com/" class="hover:underline">Nuyễn Văn Tùng</a>. All Rights Reserved.</span>
+    </div>
+</footer>
+
+
     </div>
 </template>
