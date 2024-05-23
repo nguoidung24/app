50d4b847c15026803fe9c0fa902e29c49be4cb09
dungTam <dungtamtithoi88@gmail.com>
2024-05-23 17:09:10 +0700

    fix header

--- composables/useProduct.js
+++ composables/useProduct.js
@@ -19,3 +19,24 @@ export const useProduct = async (id) => {
 
     return useState(`product_${id}`, () => data)
 }
+
+export const useProducts = async (listFilter) => {
+    const BASE_URL = (await (useBaseURL())).value.baseURL
+    let data = null;
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            action: 'listProducts',
+            ...listFilter
+        })
+    }
+
+    await fetch(BASE_URL + 'Products', requestOptions)
+        .then((response) => response.json())
+        .then((response) => data = response);
+
+    return data;
+}
